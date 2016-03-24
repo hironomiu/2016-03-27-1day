1121074a04323438f683238a8de5a3030aabc9a4
hironomiu <hironomiu@gmail.com>
2016-03-24 14:47:13 +0900

    csrf周りを除去

--- src/app/login.php
+++ src/app/login.php
@@ -1,19 +1,13 @@
 <?php
 
 session_start();
-$app->add($container->get('csrf'));
 
 $app->get('/login',function ($request, $response, $args) use($app){
     $session = $this->get('session');
-    $csrf = $this->get('csrf');
     if(!(is_null($session->get('user_id')))){
         return $response->withStatus(301)->withHeader('Location', '/');
     }
-    $name_key = $csrf->getTokenNameKey();
-    $value_key = $csrf->getTokenValueKey();
-    $name = $csrf->getTokenName();
-    $value = $csrf->getTokenValue();
-    return $this->view->render($response,'login.twig',['name_key' => $name_key,'value_key' => $value_key,'name' => $name,'value' => $value]);
+    return $this->view->render($response,'login.twig');
 });
 
 $app->post('/login',function ($request, $response, $args) {
@@ -31,20 +25,13 @@ $app->get('/logout',function ($request, $response, $args) {
     $session = $this->get('session');
     $session->destroy();
     return $response->withStatus(301)->withHeader('Location', '/');
-})->add($container->get('csrf'));
+});
 
 $app->get('/',function ($request, $response, $args) {
     $session = $this->get('session');
-    $csrf = $this->get('csrf');
     if(is_null($session->get('user_id'))){
         return $response->withStatus(301)->withHeader('Location', '/login');
     }
-    $name_key = $csrf->getTokenNameKey();
-    $value_key = $csrf->getTokenValueKey();
-    $name = $csrf->getTokenName();
-    $value = $csrf->getTokenValue();
-    echo '<input type="hidden" name="'. $name_key .'" value="'. $name .'">';
-    echo '<input type="hidden" name="'. $value_key .'" value="'. $value .'">';
     echo "hello!";
     echo $request->getQueryParams()['abc'];
 });
